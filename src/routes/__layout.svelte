f340d815b0a5099268ca3d79db56a954b0dafd35
lovaion <67899461+lovaion@users.noreply.github.com>
2022-01-06 12:11:02 +0100

    navbar changed

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
 	import Nav from '$lib/components/utilities/Nav.svelte';
 	import '../app.css';
+	
 </script>
 
 <main>
@@ -14,6 +15,7 @@
 <footer />
 
 <style>
+
 	.main-container {
 		display: flex;
 		flex-direction: column;
@@ -22,4 +24,8 @@
 		justify-self: center;
 		margin: 2rem 0;
 	}
+
+	:global(:root) {
+    --neumorphism: 5px 10px 10px rgba(0, 0, 0, 0.2), -5px -10px 10px rgba(219, 255, 255, 0.5);
+}
 </style>

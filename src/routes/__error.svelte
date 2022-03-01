b18c0989ad2e3e88d39193f56355e9133cbb6967
lovaion <67899461+lovaion@users.noreply.github.com>
2022-03-01 10:58:25 +0100

    errors

---
+++ src/routes/__error.svelte
@@ -0,0 +1,33 @@
+<script context="module">
+	export function load({ error, status }) {
+		return {
+			props: {
+				message: error.name,
+				status
+			}
+		};
+	}
+</script>
+
+<script>
+	import GenericError from '$lib/components/errors/GenericError.svelte';
+	import NotFound from '$lib/components/errors/NotFound.svelte';
+
+	export let message;
+	export let status;
+    
+</script>
+
+<h1>Qualcosa è andato storto..</h1>
+{#if status == 404}
+	<NotFound {message}/>
+{:else}
+	<GenericError {message}/>
+{/if}
+
+<style>
+	h1 {
+		font-size: 3em;
+		text-align: center;
+	}
+</style>
